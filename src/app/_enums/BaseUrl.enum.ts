9744c55da576c8ebade43f32695fd1f75d6618ef
Thanos Ntouzidis <thanosntouzidis@gmail.com>
2021-12-12 14:50:40 +0200

    Remote server urls

--- src/app/_enums/BaseUrl.enum.ts
+++ src/app/_enums/BaseUrl.enum.ts
@@ -1,6 +1,6 @@
 export enum BaseUrl {
-  // B1 = 'https://crm2022.herokuapp.com',
-  B1 = 'http://localhost:8081',
-  // B2 = 'https://crm2022.herokuapp.com'
-  B2 = 'http://localhost:8081'
+  B1 = 'https://crm2022.herokuapp.com',
+  // B1 = 'http://localhost:8081',
+  B2 = 'https://crm2022.herokuapp.com'
+  // B2 = 'http://localhost:8081'
 }
